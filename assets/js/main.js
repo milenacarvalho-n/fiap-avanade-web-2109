b9793a988a9da1d674d93b05e1f6803516854e60
Milena Carvalho <nascarvalho.m@gmail.com>
2022-09-28 12:08:31 -0300

    Deploy

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// trabalhando com javascript em um arquivo externo 
+
+// const nome = 'Fiap';
+// console.log(nome, "chamando variável criada com var");
+
+
+// // variaveis criadas com var ou funções com function sofrem hoisting: são içadas para o início do código
+// escreve('Exemplo de função nominal');
+// // função nominal - udf
+// function escreve(params) {
+//     alert(params);    
+// };
