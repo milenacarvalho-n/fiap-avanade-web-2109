bfb7b6762017ec6ec4a459bedf63287f4e441e4e
Milena Carvalho <nascarvalho.m@gmail.com>
2022-09-30 11:15:51 -0300

    aprendendo operador spread

---
+++ assets/js/spread.js
@@ -0,0 +1,44 @@
+const aereasNacionas = ['Azul', 'Gol', 'Latam'];
+
+const airCompanies = [
+    ...aereasNacionas,
+    'Delta',
+    'United Airlines',
+    'Emirates',
+    'Qatar',
+    'Tap'
+];
+
+console.log(airCompanies);
+
+// atribuição por desestruturação
+
+const [azul, voeGol, latam, delta, ...internacionais] = airCompanies;
+
+console.log(internacionais, azul, voeGol);
+
+const user = {
+    name: 'Milena Carvalho',
+    email: 'nascarvalho.m@gmail.com',
+    password: '1234'
+}
+
+const {password, ...data} = user;
+
+console.log(data, 'retornando dados da API');
+
+// eventos - Javascript é uma linguagem dirigida a eventos. Algo acontece, então reagimos
+
+//eventos - JavaScript é uma linguagem dirigida a eventos. Algo acontece, então reagimos.
+// Eventos de tempo - setTimeout, setInterval
+// Eventos de DOM - onClick, onChange, onSubmit
+// Eventos de teclado - onKeyDown, onKeyUp, onKeyPress
+// Eventos de mouse - onMouseDown, onMouseUp, onMouseMove
+// Eventos de formulário - onFocus, onBlur, onChange
+// Eventos de mídia - onPlay, onPause, onEnded
+// Eventos de rede - onLoad, onError, onAbort
+
+
+
+// dominar a linguagem
+// funções, arrays, eventos, assíncrono , estrutura de controle e loops, objetos, controle de fluxo
