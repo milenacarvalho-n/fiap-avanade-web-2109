d15a2423a808cacdf76f73f85fb5869c994b075d
Milena Carvalho <nascarvalho.m@gmail.com>
2022-10-05 09:45:51 -0300

    Ajustes Gerais

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,4 +1,4 @@
-const tooltip = document.querySelector('#tooltip');
+// const tooltip = document.querySelector('#tooltip');
 const body = document.querySelector('body');
 const header = document.querySelector('.top-site');
 
@@ -29,14 +29,14 @@ tooltip.addEventListener('mouseover', (e) => {
     newDiv.innerHTML =
     "Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii."
 
-    // newDiv.style.position = "fixed";
-    // newDiv.style.width = 200 + "px";
-    // newDiv.style.top = e.clientY + 'px';
-    // newDiv.style.display = "flex";
+    newDiv.style.position = "fixed";
+    newDiv.style.width = 200 + "px";
+    newDiv.style.top = e.clientY + 'px';
+    newDiv.style.display = "flex";
 
-    // newDiv.style.left =( e.clientX + 200 > body.clientWidth ? e.clientX - 210 : e.client + 200 ) + 'px';
+    newDiv.style.left =( e.clientX + 200 > body.clientWidth ? e.clientX - 210 : e.client + 200 ) + 'px';
 
-    // header.appendChild(newDiv);
+    header.appendChild(newDiv);
 
     const largura = 200;
     newDiv.style.position = "fixed";
@@ -63,3 +63,40 @@ body.addEventListener('mouseout', ()=>{
 
 // desafio 
 // tooltip("#tooltip", "texto do tooltip")
+
+function tooltip2(seletor, text){
+    const alvo = document.querySelector(seletor);
+    alvo.addEventListener('mouseover', (e)=>{
+        const newDiv2 = document.createElement('div');
+        newDiv2.classList.add('tooltip');
+        newDiv2.innerHTML = text;
+    
+        const largura = 200;
+        newDiv2.style.position = "fixed";
+        newDiv2.style.width = largura + "px";
+        newDiv2.style.top = e.clientY + "px";
+        newDiv2.style.display = "flex";
+        newDiv2.style.left =
+            (e.clientX + largura > body.clientWidth
+            ? e.clientX - largura
+            : e.clientX + largura) + "px";
+    
+        console.log(newDiv2.style.width, "width");
+    
+        header.appendChild(newDiv2);
+    });
+
+  
+
+    if(document.querySelector(".tooltip")){
+        document.querySelector(".tooltip").remove();
+    }
+
+    alvo.addEventListener('mouseout', ()=>{
+        if(document.querySelector('.tooltip')){
+            document.querySelector('.tooltip').remove();
+        }
+    });
+}
+
+tooltip2("#tooltip", "Minions ipsum gelatooo uuuhhh para tú bappleees para tú tank yuuu! Gelatooo po kass. Bappleees poopayee tulaliloo pepete belloo! Wiiiii. Baboiii hana dul sae bappleees.");
